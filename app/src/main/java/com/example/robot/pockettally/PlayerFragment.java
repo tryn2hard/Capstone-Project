f410a74a78ba17ba4fe431b4d87510d73d8d5807
Rob Quick <requick1031@gmail.com>
2018-06-19 14:10:22 +0900

    double and triple marks added

--- app/src/main/java/com/example/robot/pockettally/PlayerFragment.java
+++ app/src/main/java/com/example/robot/pockettally/PlayerFragment.java
@@ -3,6 +3,7 @@ package com.example.robot.pockettally;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.RotateDrawable;
@@ -10,9 +11,13 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.res.ResourcesCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,6 +40,9 @@ public class PlayerFragment extends Fragment {
     private final static int TALLY_MARK_15_VALUE = 15;
     private final static int TALLY_MARK_BULLS_VALUE = 25;
     private final static int MAX_NUM_OF_TALLY_MARKS = 3;
+    private final static int SINGLE_TALLY_MARK = 1;
+    private final static int DOUBLE_TALLY_MARK = 2;
+    private final static int TRIPLE_TALLY_MARK = 3;
 
 
     // Binding the image views
@@ -88,91 +96,280 @@ public class PlayerFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker20Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker20Count = tallyImageSelector(marker20Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_20_VALUE);
-                }
+                marker20Count = marker20Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker20Count, scoreboard_20_iv);
             }
+        });
+
+        scoreboard_20_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker20Count = marker20Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker20Count, scoreboard_20_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker20Count = marker20Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker20Count, scoreboard_20_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_19_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker19Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker19Count = tallyImageSelector(marker19Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_19_VALUE);
-                }
+                marker19Count = marker19Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker19Count, scoreboard_19_iv);
             }
+        });
+
+        scoreboard_19_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker19Count = marker19Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker19Count, scoreboard_19_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker19Count = marker19Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker19Count, scoreboard_19_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_18_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker18Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker18Count = tallyImageSelector(marker18Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_18_VALUE);
-                }
+                marker18Count = marker18Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker18Count, scoreboard_18_iv);
             }
+        });
+
+        scoreboard_18_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker18Count = marker18Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker18Count, scoreboard_18_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker18Count = marker18Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker18Count, scoreboard_18_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_17_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker17Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker17Count = tallyImageSelector(marker17Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_17_VALUE);
-                }
+                marker17Count = marker17Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker17Count, scoreboard_17_iv);
             }
+        });
+
+        scoreboard_17_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker17Count = marker19Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker17Count, scoreboard_17_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker17Count = marker17Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker17Count, scoreboard_17_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_16_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker16Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker16Count = tallyImageSelector(marker16Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_16_VALUE);
-                }
+                marker16Count = marker16Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker16Count, scoreboard_16_iv);
             }
+        });
+
+        scoreboard_16_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker16Count = marker16Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker16Count, scoreboard_16_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker16Count = marker16Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker16Count, scoreboard_16_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_15_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(marker15Count < MAX_NUM_OF_TALLY_MARKS) {
-                    marker15Count = tallyImageSelector(marker15Count, view);
-                }
-                else {
-                    addScore(TALLY_MARK_15_VALUE);
-                }
+                marker15Count = marker15Count + SINGLE_TALLY_MARK;
+                tallyImageSelector(marker15Count, scoreboard_15_iv);
             }
+        });
+
+        scoreboard_15_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker15Count = marker15Count + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(marker15Count, scoreboard_15_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        marker15Count = marker15Count + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(marker15Count, scoreboard_15_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         scoreboard_bulls_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vibe.vibrate(100);
-                if(markerBullsCount < MAX_NUM_OF_TALLY_MARKS) {
-                    markerBullsCount = tallyImageSelector(markerBullsCount, view);
-                }
-                else {
-                    addScore(TALLY_MARK_BULLS_VALUE);
-                }
+                markerBullsCount = markerBullsCount + SINGLE_TALLY_MARK;
+                tallyImageSelector(markerBullsCount, scoreboard_bulls_iv);
             }
+        });
+
+        scoreboard_bulls_iv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                final Dialog multiMarkDialog = new Dialog(getActivity());
+                multiMarkDialog.setContentView(R.layout.multiple_marks_dialog);
+                Button doubleMarkButton = multiMarkDialog.findViewById(R.id.double_mark_button);
+                doubleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        markerBullsCount = markerBullsCount + DOUBLE_TALLY_MARK;
+                        tallyImageSelector(markerBullsCount, scoreboard_bulls_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+
+                Button tripleMarkButton = multiMarkDialog.findViewById(R.id.triple_mark_button);
+                tripleMarkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        markerBullsCount = markerBullsCount + TRIPLE_TALLY_MARK;
+                        tallyImageSelector(markerBullsCount, scoreboard_bulls_iv);
+                        multiMarkDialog.dismiss();
+                    }
+                });
+                multiMarkDialog.show();
+                return false;
+            }
+
         });
 
         player_avatar_iv.setOnClickListener(new View.OnClickListener() {
@@ -270,36 +467,76 @@ public class PlayerFragment extends Fragment {
         return rootView;
     }
 
-    private int tallyImageSelector(int count, View view) {
+    private void tallyImageSelector(int count, View view) {
 
-        switch (count) {
-            case 0:
-                view.setBackground(ResourcesCompat.getDrawable(getResources(),
-                        R.drawable.tally_one_mark, null));
-                count++;
-                break;
+        if(count <= MAX_NUM_OF_TALLY_MARKS) {
 
-            case 1:
-                view.setBackground(ResourcesCompat.getDrawable(getResources(),
-                        R.drawable.tally_two_marks, null));
-                count++;
-                break;
+            switch (count) {
+                case SINGLE_TALLY_MARK:
+                    view.setBackground(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.tally_one_mark, null));
 
-            case 2:
-                view.setBackground(ResourcesCompat.getDrawable(getResources(),
-                        R.drawable.tally_three_marks, null));
-                count++;
-                break;
+                    break;
+
+                case DOUBLE_TALLY_MARK:
+                    view.setBackground(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.tally_two_marks, null));
+
+                    break;
+
+                case TRIPLE_TALLY_MARK:
+                    view.setBackground(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.tally_three_marks, null));
+
+                    break;
+
+            }
+        }else{
+            view.setBackground(ResourcesCompat.getDrawable(getResources(),
+                    R.drawable.tally_three_marks, null));
+            int overflow = count - MAX_NUM_OF_TALLY_MARKS;
+            int id = view.getId();
+            addScore(overflow, id);
 
         }
-        return count;
+
     }
 
-    private void addScore(int value){
-        totalScore = totalScore + value;
+    private void addScore(int overflow, int id){
+        switch(id){
+            case R.id.scoreboard_20:
+                totalScore = totalScore + TALLY_MARK_20_VALUE * overflow;
+                marker20Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_19:
+                totalScore = totalScore + TALLY_MARK_19_VALUE * overflow;
+                marker19Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_18:
+                totalScore = totalScore + TALLY_MARK_18_VALUE * overflow;
+                marker18Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_17:
+                totalScore = totalScore + TALLY_MARK_17_VALUE * overflow;
+                marker17Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_16:
+                totalScore = totalScore + TALLY_MARK_16_VALUE * overflow;
+                marker16Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_15:
+                totalScore = totalScore + TALLY_MARK_15_VALUE * overflow;
+                marker15Count = MAX_NUM_OF_TALLY_MARKS;
+                break;
+            case R.id.scoreboard_bulls:
+                totalScore = totalScore + TALLY_MARK_BULLS_VALUE * overflow;
+                markerBullsCount = MAX_NUM_OF_TALLY_MARKS;
+                break;
+        }
         String totalScoreString = String.valueOf(totalScore);
         game_score_tv.setText(totalScoreString);
     }
+
 
 }
 
