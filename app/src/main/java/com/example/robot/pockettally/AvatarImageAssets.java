5ea0ee9e29438bbb8a6111e1260564c12da87a4e
Rob Quick <requick1031@gmail.com>
2018-06-22 14:27:41 +0900

    reduced image sizes

--- app/src/main/java/com/example/robot/pockettally/AvatarImageAssets.java
+++ app/src/main/java/com/example/robot/pockettally/AvatarImageAssets.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class AvatarImageAssets {
 
     private static final List<Integer> avatars = new ArrayList<Integer>() {{
-        add(R.drawable.man);
-        add(R.drawable.man_1);
-        add(R.drawable.man_2);
-        add(R.drawable.man_3);
-        add(R.drawable.man_4);
-        add(R.drawable.man_5);
-        add(R.drawable.man_6);
-        add(R.drawable.woman);
-        add(R.drawable.woman_1);
+        add(R.mipmap.man);
+        add(R.mipmap.man_1);
+        add(R.mipmap.man_2);
+        add(R.mipmap.man_3);
+        add(R.mipmap.man_4);
+        add(R.mipmap.man_5);
+        add(R.mipmap.man_6);
+        add(R.mipmap.woman);
+        add(R.mipmap.woman_1);
     }};
 
     public static List<Integer> getAvatars(){ return avatars; }
